cb0fdc4165650c14739b393fbec7be5201576b88
TheAkseon <abqwerty390@gmail.com>
2021-08-23 21:45:53 +0300

    init

--- PhysicalInteraction.cs
+++ PhysicalInteraction.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PhysicalInteration : MonoBehaviour
+public class PhysicalInteraction : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
