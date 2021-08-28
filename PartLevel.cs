6b6dd4fb76e5e946da0e2700ef565cef9f62f56a
TheAkseon <abqwerty390@gmail.com>
2021-08-28 20:29:33 +0300

    init

--- PartLevel.cs
+++ PartLevel.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnPlace : MonoBehaviour
+public class PartLevel : MonoBehaviour
 {
     [SerializeField] private Begin _begin;
     [SerializeField] private End _end;
