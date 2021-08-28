b53bcbe86aeec7086d9c1f1445f62fe84178dd92
TheAkseon <abqwerty390@gmail.com>
2021-08-28 19:06:40 +0300

    init

--- SpawnPlace.cs
+++ SpawnPlace.cs
@@ -4,15 +4,6 @@ using UnityEngine;
 
 public class SpawnPlace : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] private Begin _begin;
+    [SerializeField] private End _end;
 }
